adf7d42d4ffee7582f05538d25979d28113621d7
snsinfu <snsinfu@gmail.com>
2026-01-07 15:05:04 +0900

    Add overrides.mk

---
+++ src/simulation/overrides.mk
@@ -0,0 +1,20 @@
+ENV_PREFIX = $(CONDA_PREFIX)
+
+CXX = $(ENV_PREFIX)/bin/clang++
+
+CXXFLAGS += \
+  -stdlib=libc++ \
+  -isystem   '$(ENV_PREFIX)/include/c++/v1' \
+  -isystem   '$(ENV_PREFIX)/include' \
+  -L         '$(ENV_PREFIX)/lib' \
+  -Wl,-rpath,'$(ENV_PREFIX)/lib' \
+  -Wno-unused-command-line-argument
+
+OPTFLAGS += \
+  -march=x86-64-v3 \
+  -mtune=znver2 \
+  -flto \
+  -fuse-ld=lld
+
+DBGFLAGS += \
+  -DNDEBUG
